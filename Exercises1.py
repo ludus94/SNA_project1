27cb7ecdec83acd8841d811b957b38e5e97311c1
ludus94 <l.russo62@studenti.unisa.it>
2021-04-21 11:53:57 +0200

    Graph lod work

--- Exercises1.py
+++ Exercises1.py
@@ -4,12 +4,13 @@ import networkx as nx
 from priorityq import PriorityQueue
 import random
 from scipy.sparse import linalg
-Data = open('musae_facebook_edges.csv', "r")
-next(Data, None)  # skip the first line in the input file
-Graphtype = nx.Graph()
-G = nx.parse_edgelist(Data, delimiter=',', create_using=Graphtype,
+def load_graph():
+    Data = open('musae_facebook_edges.csv', "r")
+    next(Data, None)  # skip the first line in the input file
+    Graphtype = nx.Graph()
+    G = nx.parse_edgelist(Data, delimiter=',', create_using=Graphtype,
                       nodetype=int)
-print(G)
+    return G
 def hierarchical(G):
     # Create a priority queue with each pair of nodes indexed by distance
     pq = PriorityQueue()
